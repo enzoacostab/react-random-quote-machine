2e7f7bee4c3afe5f5a5c69b9719261e315586ecb
Enzo Acosta <enzoacostab@gmail.com>
2023-05-26 11:42:12 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState} from 'react';
 
 function App() {
+  const [quote, updQuote]=useState({});
+  useEffect(()=>{
+    getQuote();
+  },[]);
+
+  const getQuote=()=>{
+    fetch('https://api.quotable.io/random')
+    .then((Response)=>{return Response.json()})
+    .then((data)=>{updQuote({text:data.content, author:data.author});
+    });
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div id='quote-box'>
+        <h2 id='text'>{quote.text}</h2>
+        <p id='author'>{"-"+quote.author}</p>
+        <div id='b'>
+        <a href={'https://twitter.com/intent/tweet?text="'+ quote.text+'" -'+quote.author} target='blank' id='tweet-quote'><i className='fa-brands fa-twitter'></i></a>
+        <button onClick={getQuote} id='new-quote'>New quote</button>
+        </div>
+      </div>
+    </div>)
 }
 
 export default App;
